a223b33e7a2473778d43cd7bd33b8cb00ff81a04
Santosh potu <santoshreddy.potu@gmail.com>
2018-04-08 23:10:04 +0530

    First Commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Title as Title;
+
+class ClientController extends Controller
+{
+    //
+    public function __construct( Title $titles )
+    {
+        $this->titles = $titles->all();
+    }
+
+    public function di()
+    {
+        dd($this->titles);
+    }
+
+    public function index()
+    {
+        return view('client/index');
+    }
+
+    public function newClient()
+    {
+        return view('client/newClient');
+    }
+
+    public function create()
+    {
+            return view('client/create');
+    }
+
+    public function show($client_id)
+    {
+        return view('client/show');
+    }
+}
