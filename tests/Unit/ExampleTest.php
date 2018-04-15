44c1bc1c52c7fb58a486fa977c5429d418045665
Santosh <santoshreddy.potu@gmail.com>
2018-04-15 10:19:22 +0530

    Unit test & functional tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Title;
 
 class ExampleTest extends TestCase
 {
@@ -16,5 +17,20 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
+    }
+    
+    public function testTitlesModelCount(){
+        $title = new Title();
+        //$value = 1;
+        //$this->assertTrue(1 == $value ,'Value Should be 1');
+        
+        $this->assertTrue(count($title->all()) == 6,'It should have 6 Titles');
+    }
+    
+    public function testLastTitleShouldBeProfession() 
+    {
+        $title = new Title;
+        $titles_array =  $title->all();
+        $this->assertEquals('Professor', array_pop($titles_array),'Titles last element should be Professor');
     }
 }
